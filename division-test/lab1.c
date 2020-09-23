e9cdcf298caee3a6464785ab6a21940cd7b71bb2
Samstep <Samstep2012@gmail.com>
2020-09-22 22:52:40 -0700

    cleanup

--- division-test/lab1.c
+++ division-test/lab1.c
@@ -1,20 +1,39 @@
+/*
+Division Test
+by Sam
+COEN 11
+*/
+
 #include <stdio.h>
-int main() {
- int i;
- int score = 0;
- srand ((int) time(NULL));
- for(i = 0; i < 10; i++) {
- int divisor = rand() % 12 + 1;
- int quotient = rand() % 13;
- printf("[#%d] Dividend: %d || Denominator: %d\n", i+1, divisor*quotient, divisor);
- int answer;
- scanf("%d", &answer);
- if(answer == quotient) { 
-   score++;
-  } else { 
-  printf("Incorrect, correct quotient is: %d\n", quotient);
+int main()
+{
+  //declare vars
+  int i;
+  int score = 0;
+  srand((int)time(NULL));
+  //loop 10 times
+  for (i = 0; i < 10; i++)
+  {
+    int divisor = rand() % 12 + 1;
+    int quotient = rand() % 13;
+    //give the user the numbers to divide
+    printf("[#%d] Dividend: %d || Denominator: %d\n", i + 1, divisor * quotient, divisor);
+    //prompt the answer
+    int answer;
+    scanf("%d", &answer);
+    //check if the answer is right
+    if (answer == quotient)
+    {
+      //if the score is right, add a point
+      score++;
+    }
+    else
+    {
+      //if the score is wrong, tell them then give them the correct answer
+      printf("Incorrect, correct quotient is: %d\n", quotient);
+    }
   }
- }
- printf("Your total score is %d/10\n", score);
- return 0;
+  //give user their final score
+  printf("Your total score is %d/10\n", score);
+  return 0;
 }
